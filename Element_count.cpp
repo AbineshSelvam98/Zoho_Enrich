a4a97b1853ae114386c5d61beca11614c8bf74ec
Abinesh S <34070629+AbineshSelvam98@users.noreply.github.com>
2018-03-04 18:19:35 +0530

    Create Element_count.cpp

---
+++ Element_count.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    int n,a[100],set[100],top=0;
+    cin>>n;
+    for(int i=0;i<n;i++) //This loop is to eliminate the duplicates and store in new array
+    {
+        cin>>a[i];
+        int l=top;
+        while(l>=0)
+        {
+            if(a[i]==set[l])
+            {
+                break;
+            }
+            l--;
+        }
+        if(l==-1)
+        {
+            set[top++]=a[i];
+        }
+        
+    }
+    for(int i=0;i<top;i++)
+    {   int count=0;
+        cout<<set[i]<<"-->";
+        for(int j=0;j<n;j++)
+        {
+            if(set[i]==a[j])
+            {
+                count++;
+            }
+            
+        }
+        cout<<count<<endl;
+    }
+}
